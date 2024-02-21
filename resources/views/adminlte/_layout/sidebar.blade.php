e2ffdc06e4e51fa788c3095e8fafea037e0b1ecb
langnang <langnang.chen@outlook.com>
2024-02-21 13:31:47 +0800

    清除多余build文件夹

--- resources/views/adminlte/_layout/sidebar.blade.php
+++ resources/views/adminlte/_layout/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
-  <a href="index3.html" class="brand-link text-center">
+  <a href="/adminlte" class="brand-link text-center">
     @empty($module_config['logo'])
     @else
       <img src="{{ $module_config['logo'] }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
