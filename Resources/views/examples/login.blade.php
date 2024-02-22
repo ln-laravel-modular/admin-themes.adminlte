01e76c5e9e9f2bff4bf817440625dba1e8e2219f
langnang <langnang.chen@outlook.com>
2024-02-22 19:09:40 +0800

    login

--- Resources/views/examples/login.blade.php
+++ Resources/views/examples/login.blade.php
@@ -22,7 +22,8 @@
       <div class="card-body login-card-body">
         <p class="login-box-msg">Sign in to start your session</p>
 
-        <form action="../../index3.html" method="post">
+        <form action="{{ $action ?? '' }}" method="post">
+          @csrf
           <div class="input-group mb-3">
             <input type="email" class="form-control" placeholder="Email">
             <div class="input-group-append">
